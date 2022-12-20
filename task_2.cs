15a2b84e970c2e6c07ad3c9cbdef6c81301ff338
Kimskaya <120120877+Kimskaya@users.noreply.github.com>
2022-12-20 16:33:48 +0300

    Update task_2.cs

--- task_2.cs
+++ task_2.cs
@@ -1,1 +1,30 @@
+// task_2. Program that takes 2 numbers and shows the biggest
+Console.WriteLine("Input number");
+int numberX = int.Parse  (Console.ReadLine()!);
 
+Console.WriteLine("Input number");
+int numberY = int.Parse (Console.ReadLine()!);
+if (numberX>numberY)
+
+Console.WriteLine(numberX+ " is max");
+else
+Console.WriteLine(numberY+" is max");
+
+//task_4. Program takes 3 numbers and outputs the biggest
+Console.WriteLine("Input number");
+int number1 = int.Parse  (Console.ReadLine()!);
+
+Console.WriteLine("Input number");
+int number2 = int.Parse (Console.ReadLine()!);
+
+Console.WriteLine("input number");
+int number3 = int.Parse(Console.ReadLine()!);
+if (number1>number2)
+
+Console.WriteLine(number1+ " is max");
+else if (number2 > number3)
+Console.WriteLine(number2+" is max");
+else 
+Console.WriteLine(number3+" is max");
+
+// task 
