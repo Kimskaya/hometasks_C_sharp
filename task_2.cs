9ad68797ca7ec8287dcf4c44404aa09c86a434c7
Kimskaya <120120877+Kimskaya@users.noreply.github.com>
2022-12-21 12:32:59 +0300

    Update task_2.cs

--- task_2.cs
+++ task_2.cs
@@ -27,4 +27,26 @@ Console.WriteLine(number2+" is max");
 else 
 Console.WriteLine(number3+" is max");
 
-// task 
+// task_6 program takes a number and outputs 
+//if the number is ordinary
+
+Console.WriteLine("Input number");
+int numberA = int.Parse  (Console.ReadLine()!);
+if (numberA%2==0)
+Console.WriteLine(numberA+" is an ordinary number");
+else 
+Console.WriteLine(numberA+ " is an odd number");
+//task_8 program takes a number 
+//and shows all the ordinary numbers from 1 to number
+
+Console.WriteLine("Input number");
+int numberOrd = int.Parse (Console.ReadLine()!);
+
+while(numberOrd>=1)
+
+if (numberOrd%2==0)
+    Console.WriteLine(numberOrd);
+    numberOrd = numberOrd-2;
+    else 
+numberOrd = numberOrd-1;
+Console.WriteLine("NumberOrd");
